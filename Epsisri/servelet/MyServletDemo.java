c1c53936440c3f0f46b702f7704704855e6b42bf
Epsisri <56478342+Epsisri@users.noreply.github.com>
2020-11-05 18:25:50 +0530

    Add: Epsisri Report

---
+++ Epsisri/servelet/MyServletDemo.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class to create Http Servlet
+public class MyServletDemo extends HttpServlet {
+
+   private String mymsg;
+
+   public void init() throws ServletException {
+      mymsg = "This is Epsisri Potluri, RA1711003011248.";
+   }
+
+   public void doGet(HttpServletRequest request, 
+      HttpServletResponse response)
+      throws ServletException, IOException 
+   {
+
+      // Setting up the content type of webpage
+      response.setContentType("text/html");
+
+      // Writing message to the web page
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + mymsg + "</h1>");
+   }
+
+   public void destroy() {
+      /* leaving empty for now this can be
+       * used when we want to do something at the end
+       * of Servlet life cycle
+       */
+   }
+}
